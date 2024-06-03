07f2b75cfef600418fa5f488e9ac829ec80916a2
r.ciesielski3 <r.ciesielski3@gmail.com>
2024-06-03 19:15:57 +0200

    refactor server.js script

--- BE-service/server.js
+++ BE-service/server.js
@@ -1,11 +1,15 @@
 const jsonServer = require("json-server");
+const path = require("path");
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
-const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
-  console.log("JSON Server is running on port", PORT);
+  console.log(`JSON Server is running on port ${PORT}`);
 });
